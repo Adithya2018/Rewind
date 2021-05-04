9c376b73286c3a897e768acaf3f5538ae05031d3
Adithyan S Raj <adithyansraj20@gmail.com>
2021-05-04 20:47:07 +0530

    state management dart

--- rewind_app/lib/main.dart
+++ rewind_app/lib/main.dart
@@ -25,7 +25,7 @@ class RewindApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: JournalTest(),
+      home: Home(),
       routes: <String, WidgetBuilder>{
         '/ach': (BuildContext context) => Achievements(),
         '/jou': (BuildContext context) => Journal(),
@@ -35,6 +35,7 @@ class RewindApp extends StatelessWidget {
         '/add': (BuildContext context) => CreateTask(),
         '/vt': (BuildContext context) => EditTask(),
       },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
